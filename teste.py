a2254c38355e46cf6592473bc47908000830619b
henricobela <henrico.developer@gmail.com>
2023-08-29 16:28:41 -0300

    teste

--- teste.py
+++ teste.py
@@ -4,13 +4,14 @@ born = 1996
 now = datetime.now()
 age = now.year - born
 
+
 def henrico() -> dict:
     return {
-      "nome": "Henrico Nardelli Bela",
-      "idade": age,
-      "hobbies": ["Programacao", "Cinema", 
-                  "Series", "Games"]
+        "nome": "Henrico Nardelli Bela",
+        "idade": age,
+        "hobbies": ["Programacao", "Cinema", "Series", "Games"],
     }
-    
+
+
 nome, idade, hobbies = henrico().values()
 print(f"Nome: {nome}\nIdade: {idade}\nHobbies: {hobbies}")
